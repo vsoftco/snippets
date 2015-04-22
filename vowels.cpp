b5c61e161b0654b49f2d8e3ca9657c9ca2aeffae
vsoftco <vsoftco@gmail.com>
2015-04-22 19:15:48 -0400

    commit

---
+++ vowels.cpp
@@ -0,0 +1,21 @@
+// Count the vowels in a string
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <cstddef>
+
+int main()
+{
+    std::map<char, std::size_t> m;
+    std::string s{"This is some test with vowels"};
+
+    for(auto x: s)
+        m[x]++;
+
+    std::cout << "a: " << m['a'] << std::endl;
+    std::cout << "e: " << m['e'] << std::endl;
+    std::cout << "i: " << m['i'] << std::endl;
+    std::cout << "o: " << m['o'] << std::endl;
+    std::cout << "u: " << m['u'] << std::endl;
+}
