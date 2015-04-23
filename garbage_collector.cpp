d1582763b45dd3dbf0d086af7b1dd0bac9b36183
vsoftco <vsoftco@gmail.com>
2015-04-23 15:29:41 -0400

    commit

--- garbage_collector.cpp
+++ garbage_collector.cpp
@@ -6,19 +6,22 @@
 
 class GC
 {
-    // globally allocated memory map
-    static std::map<void*, std::pair<std::size_t, bool>> memory;
+    // globally allocated _memory map
+    static std::map<void*, std::pair<std::size_t, bool>> _memory;
 
 public:
     struct collect_t {}; // tag for placement new
     static void display_memory();
     static void clear_memory();
+    static bool is_managed(void* p);
+    static void remove(void *p);
+    static std::pair<std::size_t, bool> get(void* p);
     ~GC();
     friend void* operator new(std::size_t size, const GC::collect_t&,
                               bool is_array);
     friend void operator delete(void *p, const GC::collect_t&) noexcept;
 };
-std::map<void*, std::pair<std::size_t, bool>> GC::memory;
+std::map<void*, std::pair<std::size_t, bool>> GC::_memory;
 
 void* operator new(std::size_t size, const GC::collect_t&,
                    bool is_array = false);
@@ -34,13 +37,28 @@ int main()
     char *c = new(GC::collect_t{}) char;
     int *p = new(GC::collect_t{}) int[1024]; // true size: sizeof(int)*100 + y
 
+    // don't use the garbace collector
+    int *tmp = new int;
+
+    std::cout << std::boolalpha << GC::is_managed(c) << std::endl;
+    std::cout << std::boolalpha << GC::is_managed(p) << std::endl;
+    std::cout << std::boolalpha << GC::is_managed(tmp) << std::endl;
+
     GC::display_memory();
+
+    GC::remove(c);
+    GC::display_memory();
+    std::cout << std::boolalpha << GC::is_managed(c) << std::endl;
+    std::cout << std::boolalpha << GC::is_managed(p) << std::endl;
+    std::cout << std::boolalpha << GC::is_managed(tmp) << std::endl;
+
+    delete tmp;
 }
 
 void GC::display_memory()
 {
     std::cout << "Allocated: " << std::endl;
-    for (auto && elem : memory)
+    for (auto && elem : _memory)
     {
         std::cout  << std::setfill('.')
                    << "\tADDR: "  << std::setw(8)
@@ -52,28 +70,46 @@ void GC::display_memory()
     }
 }
 
-GC::~GC()
-{
-    clear_memory();
-    display_memory();
-}
-
 void GC::clear_memory()
 {
-    for (auto && elem : memory)
+    for (auto && elem : _memory)
     {
         if (elem.second.second) // non-array
             operator delete(elem.first, GC::collect_t{});
         else
             operator delete[](elem.first, GC::collect_t{});
     }
-    memory.clear();
+    _memory.clear();
+}
+
+GC::~GC()
+{
+    clear_memory();
+    display_memory();
+}
+
+bool GC::is_managed(void* p)
+{
+    return _memory.find(p) != _memory.end();
+}
+
+void GC::remove(void *p)
+{
+    _memory.erase(p);
+}
+
+std::pair<std::size_t, bool> GC::get(void* p)
+{
+    if (is_managed(p))
+        return _memory[p];
+    else
+        return std::make_pair(0, false);
 }
 
 void* operator new(std::size_t size, const GC::collect_t&, bool is_array)
 {
     void* addr = ::operator new(size);
-    GC::memory[addr] = std::make_pair(size, is_array);
+    GC::_memory[addr] = std::make_pair(size, is_array);
     return addr;
 }
 
@@ -84,7 +120,7 @@ void* operator new[](std::size_t size, const GC::collect_t&)
 
 void operator delete(void *p, const GC::collect_t&) noexcept
 {
-    GC::memory.erase(p); // should call ::operator delete, no recursion
+    GC::_memory.erase(p); // should call ::operator delete, no recursion
     ::operator delete(p);
 }
 
