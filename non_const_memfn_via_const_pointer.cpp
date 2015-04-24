1ceb46f85f4e7000a50b67ef8d8e40db19afea38
vsoftco <vsoftco@gmail.com>
2015-04-23 21:15:21 -0400

    commit

---
+++ non_const_memfn_via_const_pointer.cpp
@@ -0,0 +1,31 @@
+// Invoking non-const member functions of const objects via const pointers
+// Undefined behaviour
+
+#include <iostream>
+
+struct Foo
+{
+    int x{10};
+    void f() const
+    {
+        std::cout << "Foo::f() const" << std::endl;
+    }
+
+    void f() 
+    {
+        std::cout << "Foo::f()" << std::endl;
+        ++x;
+    }
+
+    Foo* pFoo{this};
+};
+
+int main(void)
+{
+    const Foo foo;
+    
+    std::cout << foo.x << std::endl; // initially x = 10
+    static_cast<const Foo*>(foo.pFoo)->f();
+    foo.pFoo->f();
+    std::cout << foo.x << std::endl; // modified x, now x = 11
+}
