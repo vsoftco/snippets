f652b676bcd0d7235dff7044dc4ebc5954654b46
vsoftco <vsoftco@gmail.com>
2015-05-04 19:29:23 -0400

    commit

---
+++ pure_virtual_implemented.cpp
@@ -0,0 +1,37 @@
+// Pure virtual functions can be implemented!
+
+#include <iostream>
+#include <memory>
+
+struct Base
+{
+    virtual void f() = 0;
+    // we MUST implement a pure virtual destructor,
+    // since it is invoked by the compiler
+    virtual ~Base() = 0;
+};
+
+void Base::f()
+{
+    std::cout << "Base::f()" << std::endl;
+}
+
+Base::~Base()
+{
+    std::cout << "Base::~Base()" << std::endl;
+}
+
+struct Derived: Base
+{
+    void f() override
+    {
+        Base::f(); // default behaviour
+        std::cout << "Derived::f()" << std::endl;
+    }
+};
+
+int main()
+{
+    auto upBase = std::make_unique<Derived>();
+    upBase->f();
+}
