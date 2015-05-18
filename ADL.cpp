21f93e3fc2148d776579b5e80d1473431c5997e4
vsoftco <vsoftco@gmail.com>
2015-05-17 21:46:03 -0400

    commit

--- ADL.cpp
+++ ADL.cpp
@@ -5,32 +5,19 @@
 // some namespace, no need actually, can use the global one
 namespace ADL
 {
-class Foo
-{
-};
-
-void swap(Foo& lhs, Foo& rhs)
+class Foo {};
+void swap(Foo&, Foo&)
 {
     std::cout << "calling Foo::swap" << std::endl;
 }
 } /* namespace ADL */
 
-template<typename T>
-void invoke_swap(T& x)
-{
-    T y;
-
-    using std::swap; // bring ADL into play
-    swap(x, y); // looks first into the namespace x and y belong
-    // for a swap defined there that is a better match
-    // then the generic std::swap<>
-}
-
 int main()
 {
-    ADL::Foo a;
-    int i;
-
-    invoke_swap(a); // calling ADL::Foo::swap via ADL
-    invoke_swap(i); // calling std::swap
+    ADL::Foo a, b;
+    int x = 0, y = 0;
+    
+    using std::swap; // bring ADL into play
+    swap(a, b); // finds ADL::swap
+    swap(x, y); // invokes global swap
 }
