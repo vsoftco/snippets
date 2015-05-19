12effdb8c0fd1f50054859f128e5e97f65f26722
vsoftco <vsoftco@gmail.com>
2015-05-19 19:12:30 -0400

    commit

---
+++ disabling_ADL.cpp
@@ -0,0 +1,22 @@
+// Disabling ADL
+// See comment by 0x499602D2 on http://stackoverflow.com/a/30265205/3093378
+#include <iostream>
+
+namespace ADL
+{
+struct Foo {};
+void f(const Foo&)
+{
+    std::cout << "ADL::f" << std::endl;
+}
+}
+
+int main()
+{
+    Foo foo{};
+    f(ADL::Foo{}); // f found by ADL, compiles
+    
+    // below (f) is not a function call expression
+    // ADL does not kick in, and therefore it does not compile
+    (f)(ADL::Foo{}); 
+}
