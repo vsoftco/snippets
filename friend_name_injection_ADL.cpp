957cfad13c80c65415bf1e40fef28629d4abda26
vsoftco <vsoftco@gmail.com>
2015-05-19 21:28:53 -0400

    commit

---
+++ friend_name_injection_ADL.cpp
@@ -0,0 +1,21 @@
+// Friend name injection and ADL
+
+#include <iostream>
+
+struct Foo
+{
+    friend void f(const Foo&){}
+    friend void g(){}
+};
+
+int main() 
+{
+    f(Foo{}); // call succeeds due to ADL
+    
+    // line below does not compile, even if the name is "injected"
+    // into the global namespace; it remains "hidden" for the purpose
+    // of name lookup
+    // g();
+    // ::g(); // same 
+    // Foo::g(); // g is not a member of Foo
+}
