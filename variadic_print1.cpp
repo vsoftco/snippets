42f28576732fddfbd8eebbfc0425e84d97dc1ec5
vsoftco <vsoftco@gmail.com>
2015-06-01 11:20:53 -0400

    commit

---
+++ variadic_print1.cpp
@@ -0,0 +1,20 @@
+// Variadic print, take 2
+
+#include <iostream>
+
+void print()
+{
+    std::cout << std::endl;
+}
+
+template<typename Head, typename... Args>
+void print(const Head& head, const Args&... args )
+{
+    std::cout << head << " ";
+    print(args...);
+}
+
+int main()
+{
+    print("This", 2, 'x' , "is a test");
+}
---
+++ variadic_print2.cpp
@@ -0,0 +1,33 @@
+// Variadic print, take 2
+
+#include <iostream>
+#include <stdexcept>
+
+void print(const char* s)
+{
+    while (s && *s) {
+        if (*s == '%' && *++s != '%')
+            // make sure that there wasn't meant to be more arguments
+            // %% represents plain % in a format string
+            throw std::runtime_error("invalid format: missing arguments");
+        std::cout << *s++;
+    }
+}
+
+template<typename T, typename... Args>      // note the "..."
+void print(const char* s, T value, Args... args) // note the "..."
+{
+    while (s && *s) {
+        if (*s == '%' && *++s != '%') { // a format specifier (ignore which one it is)
+            std::cout << value;     // use first non-format argument
+            return print(++s, args...);  // "peel off" first argument
+        }
+        std::cout << *s++;
+    }
+    throw std::runtime_error("extra arguments provided to printf");
+}
+
+int main()
+{
+    print("OK %d test", 10);
+}
