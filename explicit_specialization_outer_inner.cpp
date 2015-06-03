798fb8d0f504b96e914bb34c76d56178e9b802ec
vsoftco <vsoftco@gmail.com>
2015-06-03 10:32:10 -0400

    commit

---
+++ explicit_specialization_outer_inner.cpp
@@ -0,0 +1,31 @@
+// Explicit specialization of member function templates 
+// of class templates
+
+#include <iostream>
+
+template<typename T>
+struct Foo
+{
+    template<typename U>
+    void test()
+    {
+        std::cout << "[T, U] generic\n";
+    }
+};
+
+template<>
+    template<>
+void Foo<int>::test<double>()
+{
+    std::cout << "[T = int, U = double] specialization\n";
+}
+
+
+int main()
+{
+    Foo<int> foo;
+    Foo<float> bar;
+    
+    foo.test<double>();
+    bar.test<float>();
+}
