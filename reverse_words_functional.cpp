1da9006364b6a9fd00885060ea12d996914df2d4
vsoftco <vsoftco@gmail.com>
2015-06-03 12:18:16 -0400

    commit

---
+++ reverse_words_functional.cpp
@@ -0,0 +1,19 @@
+// Reverting words in a sentence, functional style
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+
+int main() 
+{
+    std::stringstream input{"this is a test"};
+    std::transform(std::istream_iterator<std::string>(input), {}, 
+        std::ostream_iterator<std::string>(std::cout," "),
+        [](const std::string& str)
+        {
+            return std::string{str.rbegin(), str.rend()};
+        }
+    ); // outputs "siht si a tset "
+}
