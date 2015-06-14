6611c57464b2f96bf8f7dff8d31babf8f351eac3
vsoftco <vsoftco@gmail.com>
2015-06-14 14:28:39 -0400

    commit

--- type_list_length.cpp
+++ type_list_length.cpp
@@ -4,18 +4,15 @@
 #include <iostream>
 
 template<typename Head, typename... Args>
-struct lenght_type_list;
+struct lenght_type_list
+{
+    const static size_t length = 1 + lenght_type_list<Args...>::length;
+};
 
 template<typename Head>
 struct lenght_type_list<Head>
 {
     const static size_t length = 1;
-};
-
-template<typename Head, typename... Args>
-struct lenght_type_list
-{
-    const static size_t length = 1 + lenght_type_list<Args...>::length;
 };
 
 int main()
