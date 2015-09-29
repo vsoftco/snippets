bba8fee13bb99f3881bb5a4e9db28ca6f42febdd
vsoftco <vsoftco@gmail.com>
2015-09-29 09:57:50 -0400

    commit

--- spell.cpp
+++ spell.cpp
@@ -1,48 +1,25 @@
-// Spells a string using the phonetic alphabet
+// Spells the input using the phonetic alphabet
+// Usage: spell (end with CTRL+D on UNIX, CTRL+Z on Windows)
+// Usage: cat file > spell
 
 #include <cctype>
-#include <cstdlib>
 #include <iostream>
 #include <map>
 
-int main(int argc, char** argv)
+int main()
 {
-    if (argc != 2)
-    {
-        std::cout << "Usage: " << argv[0] << " string_to_be_translated\n";
-        std::exit(EXIT_FAILURE);
-    }
-
-    std::string str{argv[1]};
     std::map<char, std::string> dict;
-    dict['A'] = "Alpha";
-    dict['B'] = "Bravo";
-    dict['C'] = "Charlie";
-    dict['D'] = "Delta";
-    dict['E'] = "Echo";
-    dict['F'] = "Foxtrot";
-    dict['G'] = "Golf";
-    dict['H'] = "Hotel";
-    dict['I'] = "India";
-    dict['J'] = "Juliet";
-    dict['K'] = "Kilo";
-    dict['L'] = "Lima";
-    dict['M'] = "Mike";
-    dict['N'] = "November";
-    dict['O'] = "Oscar";
-    dict['P'] = "Papa";
-    dict['Q'] = "Quebec";
-    dict['R'] = "Romeo";
-    dict['S'] = "Sierra";
-    dict['T'] = "Tango";
-    dict['U'] = "Uniform";
-    dict['V'] = "Victor";
-    dict['W'] = "Whiskey";
-    dict['X'] = "X-ray";
-    dict['Y'] = "Yankee";
-    dict['Z'] = "Zulu";
+    dict['A'] = "Alpha";  dict['B'] = "Bravo";    dict['C'] = "Charlie";
+    dict['D'] = "Delta";  dict['E'] = "Echo";     dict['F'] = "Foxtrot";
+    dict['G'] = "Golf";   dict['H'] = "Hotel";    dict['I'] = "India";
+    dict['J'] = "Juliet"; dict['K'] = "Kilo";     dict['L'] = "Lima";
+    dict['M'] = "Mike";   dict['N'] = "November"; dict['O'] = "Oscar";
+    dict['P'] = "Papa";   dict['Q'] = "Quebec";   dict['R'] = "Romeo";
+    dict['S'] = "Sierra"; dict['T'] = "Tango";    dict['U'] = "Uniform";
+    dict['V'] = "Victor"; dict['W'] = "Whiskey";  dict['X'] = "X-ray";
+    dict['Y'] = "Yankee"; dict['Z'] = "Zulu";
 
-    for (auto && elem : str)
+    for (char elem; std::cin >> elem; )
     {
         std::cout << elem;
         if (std::isalpha(elem))
