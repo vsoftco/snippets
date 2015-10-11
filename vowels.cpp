1264c1966623697655e069a0677849efe11d7588
vsoftco <vsoftco@gmail.com>
2015-10-10 22:35:34 -0400

    commit

--- vowels.cpp
+++ vowels.cpp
@@ -10,8 +10,8 @@ int main()
     std::map<char, std::size_t> m;
     std::string s{"This is some test with vowels"};
 
-    for(auto x: s)
-        m[x]++;
+    for (auto x : s)
+        ++m[x];
 
     std::cout << "a: " << m['a'] << std::endl;
     std::cout << "e: " << m['e'] << std::endl;
