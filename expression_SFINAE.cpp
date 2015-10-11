7c2764c1b56ab54b02124b707fc6f80696c5cd8e
vsoftco <vsoftco@gmail.com>
2015-10-11 00:48:33 -0400

    commit

---
+++ expression_SFINAE.cpp
@@ -0,0 +1,24 @@
+// Expression SFINAE
+
+#include <iostream>
+
+struct X {};
+struct Y {
+    Y(X) {} // converting constructor
+};
+
+template <class T> auto f(T t1, T t2) -> decltype(t1 + t2) // #1
+{
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
+X f(Y, Y) // #2
+{
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+    return {};
+}
+
+int main()
+{
+    X x1, x2;
+    f(x1, x2);  // deduction fails on #1 (cannot add X + X), calls #2
+}
