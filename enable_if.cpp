ebf62af9e74ae04cb6bb5c1f379b7e321f79b8a6
vsoftco <vsoftco@gmail.com>
2016-06-20 18:20:50 -0400

    commit

---
+++ enable_if.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <type_traits>
+#include <typeinfo>
+
+template<typename T>
+std::enable_if_t<std::is_same<int,T>::value, int>
+foo(T x)
+{
+    std::cout << "Int overload\n";
+    return x;
+}
+
+template<typename T>
+std::enable_if_t<std::is_same<double,T>::value, double>
+foo(T x)
+{
+    std::cout << "Double overload\n";
+    return x;
+}
+
+int main()
+{
+    std::cout << foo(42.5) << std::endl;
+    std::cout << foo(42) << std::endl;
+    // the line below won't compile, substitution error
+    //std::cout << foo("test") << std::endl; 
+}
