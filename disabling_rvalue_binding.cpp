cca417c137187aaa984dabc15be39febbc4d7523
Vlad Gheorghiu <vsoftco@gmail.com>
2016-10-12 20:34:44 -0400

    commit

---
+++ disabling_rvalue_binding.cpp
@@ -0,0 +1,31 @@
+// Disabling rvalue binding to const references
+
+struct X {};
+
+// we intend to use this function only with lvalues
+void f(const X&) {}
+
+// a non-const rvalue factory
+X get_rvalue_X()
+{
+    return {};
+}
+
+// a const-rvalue factory
+const X get_const_rvalue_X()
+{
+    return {};
+}
+
+// below we need the const X&& as otherwise the output of the function
+// get_const_rvalue_X() will bind to void f(const X&) since the latter
+// is a better match than void f(X&&) below
+void f(const X&&) = delete; // need const!
+
+int main()
+{
+    f(get_rvalue_X());
+
+    // remove const from void f(const X&&) and the line below will compile
+    f(get_const_rvalue_X());
+}
