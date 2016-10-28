fc8c4fdd6a2c6003eb570a758632caa547dd6cf7
Vlad Gheorghiu <vsoftco@gmail.com>
2016-10-28 06:42:56 -0400

    commit

--- container_print.cpp
+++ container_print.cpp
@@ -1,15 +1,18 @@
 // Display an arbitrary iterable container in pretty format
 
 #include <iostream>
+#include <type_traits>
 #include <vector>
 
 // used in SFINAE, future std::void_t
 template<typename ...>
 using to_void = void;
 
+// iterable type trait
 template<typename T, typename = void>
 struct is_iterable : std::false_type {};
 
+// specialization for iterables
 template<typename T>
 struct is_iterable<T,
         to_void<decltype(std::declval<T>().begin()),
@@ -22,8 +25,10 @@ struct is_iterable<T,
 template<typename C>
 typename std::enable_if<is_iterable<C>::value, std::ostream&>::type
 operator<<(std::ostream& s, const C& v) {
+    // nice trick from 
+    // http://en.cppreference.com/w/cpp/container/vector/vector#Example
     s.put('[');
-    char comma[3] = {'\0', ' ', '\0'};
+    char comma[3] = {'\0', ' ', '\0'}; 
     for (const auto& e : v) {
         s << comma << e;
         comma[0] = ',';
