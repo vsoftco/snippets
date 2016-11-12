4266a90b9343c64ae80db54c0f68ce2c30d4e800
Vlad Gheorghiu <vsoftco@gmail.com>
2016-11-11 21:21:16 -0500

    commit

---
+++ max_max.cpp
@@ -0,0 +1,43 @@
+// Finds the largest and second largest values in a range
+
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <iostream>
+#include <iterator>
+#include <list>
+
+template<typename Iterator>
+auto max_max(Iterator begin, Iterator end)
+{
+    assert(std::distance(begin, end) >= 2);
+    using T = typename std::iterator_traits<Iterator>::value_type;
+
+    auto it = std::next(begin);
+    std::pair<T, T> max = std::minmax(*begin, *it);
+    while (++it != end)
+    {
+        if (*it > max.first)
+        {
+            if (*it > max.second)
+            {
+                max.first = max.second;
+                max.second = *it;
+            }
+            else
+            {
+                max.first = *it;
+            }
+        }
+    }
+
+    return max;
+}
+
+int main()
+{
+    std::list<int> v{10, -2, 15, 1, 204, -10, 17};
+
+    auto result = max_max(std::begin(v), std::end(v));
+    std::cout << result.first << ' ' << result.second;
+}
