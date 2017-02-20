08ece22520d224e02aeee190c0e3f6e7c83055c2
Vlad Gheorghiu <vsoftco@gmail.com>
2017-02-20 12:34:17 -0500

    commit

---
+++ rank.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstddef>
+
+// finds the rank of an array
+
+template<typename T>
+struct rank
+{
+    static constexpr std::size_t value = 0;
+};
+
+template<typename T>
+struct rank<T[]>
+{
+    static constexpr std::size_t value = 1 + rank<T>::value;
+};
+
+template<typename T, std::size_t N>
+struct rank<T[N]>
+{
+    static constexpr std::size_t value = 1 + rank<T>::value;
+};
+
+int main()
+{
+    std::cout << rank<int[][5][7][9]>::value << std::endl;
+}
