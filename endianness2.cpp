ed5bc048323bb5aa8683949fdee9cefdfd6ede5b
Vlad Gheorghiu <vsoftco@gmail.com>
2017-03-15 09:38:38 -0400

    commit

--- endianness2.cpp
+++ endianness2.cpp
@@ -12,7 +12,7 @@ bool is_little_endian()
     } e { 0x01234567 }; // initializes the first member, i.e. i is active
     // it's undefined behavior to read from the member of the union 
     // that wasn't most recently written. Many compilers implement,
-    //  as a non-standard language extension, the ability to read 
+    // as a non-standard language extension, the ability to read 
     // inactive members of a union. (cppreference.com)
     return (e.c[0] - 0x67 == 0);
 }
