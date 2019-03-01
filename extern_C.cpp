0119bc3093e9dc940a933bbe189cc8423ce47f01
Vlad Gheorghiu <vsoftco@gmail.com>
2019-03-01 17:11:30 -0500

    Update extern_C.cpp

--- extern_C.cpp
+++ extern_C.cpp
@@ -5,20 +5,20 @@
 
 namespace X {
 extern "C" {
-void test() { std::cout << "C linkage\n"; }
+    void test() { std::cout << "C linkage\n"; }
 }
 
 // void test() {} // won't compile!
 
-void test(int){std::cout << "C++ linkage\n";}
+void test(int) {std::cout << "C++ linkage\n";}
 } // namespace X
 
 // all extern "C" functions share the same space of names
 // so we can "bring" test() into the global namespace
-extern "C" void test(); 
+extern "C" void test();
 
 int main() {
-    X::test(); // C linkage
-    test();    // C linkage
+    X::test();   // C linkage
+    test();      // C linkage
     X::test(42); // C++ linkage
 }
