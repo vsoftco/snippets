d4972cf62f9973a8da6f600af59ae3ef3c2d7ef2
Vlad Gheorghiu <vsoftco@gmail.com>
2019-03-27 16:45:10 -0400

    Update if_constexpr.cpp

--- if_constexpr.cpp
+++ if_constexpr.cpp
@@ -17,13 +17,13 @@ void f(T x) {
     }
 }
 
-// Outside a template, a discarded statement is fully checked. if constexpr is
+// outside a template, a discarded statement is fully checked. if constexpr is
 // not a substitute for the #if preprocessing directive; won't compile as is
 /*
 void g() {
      if constexpr(false) {
         int i = 0;
-        int *p = i; // Error even though in discarded statement
+        int *p = i; // error even though in discarded statement
     }
 }
 */
